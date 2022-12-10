ffe4ebda8bc304aec60c811e71fd263ec3bbbce4
Emmanuel <alzadidine33@gmail.com>
2022-12-10 02:14:10 +0000

    2026: Forgot seperator
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-	android.hardware.fastboot@1.0-impl-mock
+	android.hardware.fastboot@1.0-impl-mock \
 	fastbootd
 
 # Additional binaries & libraries needed for recovery
