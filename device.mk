3641019d19380abd3e14b5731c78783b056a4450
Emmanuel <alzadidine33@gmail.com>
2022-12-10 01:31:38 +0000

    2026: More fix update
    Signed-off-by: Emmanuel <alzadidine33@gmail.com>

--- device.mk
+++ device.mk
@@ -24,6 +24,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 # Fastbootd
 PRODUCT_PACKAGES += \
 	android.hardware.fastboot@1.0-impl-mock
+	fastbootd
 
 # Additional binaries & libraries needed for recovery
 TARGET_RECOVERY_DEVICE_MODULES += \
